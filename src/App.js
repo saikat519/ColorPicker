7532e8c503e505e0b1ad57bdca06375b87031831
elisha-16 <ghoshelisha19@gmail.com>
2021-09-21 20:44:47 +0530

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello from Group G
-        </p>
-        <p>
-          Lets Build Color Picker
-        </p>
-
+        <Button variant="primary">Primary</Button>
+        <Button variant="warning">Warning</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
