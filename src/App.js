ca2a4e5593ca24dad80fd199e4037312faa50fb1
CaptainMist <tirthabaisya@gmail.com>
2021-09-21 21:38:15 +0530

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Button from 'react-bootstrap/Button';
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button variant="primary">Primary</Button>
-        <Button variant="warning">Warning</Button>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      
     </div>
   );
 }
